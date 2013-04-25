ad038d69de569d5944a0f3c48b5617493d234b74
Eu-wen Ding <dingeuwen@Eu-wens-MacBook-Pro.local>
2013-04-25 12:14:20 -0700

    triangle function was not working before. works now and only requires 2 sides and the angle between them, and calculates area via sine rule and heron's formula

--- triangle_area.js
+++ triangle_area.js
@@ -3,13 +3,20 @@
 // Bonus 2: Have the program calculate the lengths of the third side from the lengths of two sides. 
 
 
-var triangle = {
-	sideA: 30,
-	sideB: 40,
-	sideC: 50
-};
+function area() {
+    var sideA = parseFloat(prompt("Enter the length of the first side of the triangle", ""));
+    var sideB = parseFloat(prompt("Enter the length of the second side of the triangle", ""));
+    var angle = parseFloat(prompt("Enter the angle in degrees between these two sides (inbetween 0 and 180, exclusive)", ""));
+    var cos_angle = Math.cos(angle * (Math.PI/180));
+    var sin_angle = Math.sin(angle * (Math.PI/180));
+    var sideC = Math.sqrt((sideA * sideA) + (sideB * sideB) - (2 * sideA * sideB * cos_angle));
+    var s = (sideA + sideB + sideC) / 2;
 
-function area(triangle) {
-  var s = (triangle.sideA + triangle.sideB + triangle.sideC) / 2;
-  return Math.sqrt(s * (s - triangle.sideA) * (s - triangle.sideB) * (s - triangle.sideC));
+    var ans1 = Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+    var ans2 = (0.5)  * sideA * sideB * sin_angle;
+    console.log("Side A is of length " + sideA + ", side B is of length " + sideB + ", side C is of length " + sideC);
+    console.log("Area via Heron's forumla is " + ans1 + ".");
+    console.log("Area via sine rule is " + ans2 + ".");
 }
+
+
